cd0991eeb5e20fcbe4eddf8a8287717845016509
Tyler Holland <hollandt@sonoma.edu>
2017-03-03 15:09:24 -0800

    added fileIO and small test file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        // initialize data structure
+	String in_filename = "input.txt";
+	String out_filename = "output.txt";
+
+	if (args.length > 0)
+	    in_filename = args[0];
+	if (args.length > 1)
+	    out_filename = args[1];
+	System.out.println("source file: " + in_filename);
+	System.out.println("output file: " + out_filename);
+	
+	int size = 0;
+	int cakes[] = {};
+	Scanner in = null;
+	try {
+	    in = new Scanner(new FileReader(in_filename));
+	    size = in.nextInt();
+	    cakes = new int[size];
+	    for (int i = 0; i < size; i++) {
+		cakes[i] = in.nextInt();
+	    }
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	    System.out.println("Error: failed to open file");
+	    System.exit(1);
+	} catch (InputMismatchException e) {
+	    e.printStackTrace();
+	    System.out.println("Error: input improperly formatted");
+	    System.exit(1);
+	}
+
+	System.out.print("The list:");
+	for (int i = 0; i < size; i++)
+	    System.out.print(" " + cakes[i]);
+	System.out.println();
+
+	// create root node
+	// do the A* search
+	// write the output to out_file
+	// 	sequence of reversals to complete the sort
+	//	number of nodes expanded in the search
+	//		(aka nodes deleted from the OPEN set)
     }
 }
