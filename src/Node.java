ba570f61372bdc190ab2a773f8603110365a5e46
Jeffrey Booher-Kaeding <jeffbk95@gmail.com>
2017-03-03 16:43:24 -0800

    Node class fledged out, will still need to implement more functions though.

--- src/Node.java
+++ src/Node.java
@@ -1,18 +1,56 @@
+import java.util.ArrayList;
+
 /**
  * Created by jeffdev on 3/3/17.
  */
 public class Node {
 
+    //number of break points in our stack
     private int numBreakpoint;
+    private int cost;
+    private Node parent;
+
+    //Our pancake stack
     private int stack[];
 
-    public Node(int i, int a[]){
-        numBreakpoint = i;
-        stack = a;
+    public Node(int stack[], int cost, Node parent){
+    //constructor
+        this.cost = cost;
+        this.stack = stack;
+        this.parent = parent;
+
+
+    }
+
+    public ArrayList genChildren(){
+        ArrayList<Node> children = new ArrayList<Node>();
+
+        for (int i = 0; i < stack.length - 2; i++)
+            for ( int j = i; j <stack.length - 2 ; j++)
+                if(stack[j] > stack[j+1])
+                    children.add(new Node(flipBreakpoint(i,j),cost+1,this));
+        return children;
+    };
+
+
+    private int[] flipBreakpoint(int start, int end){
+        //True means there is a break point
+        int tempStack[] = stack;
+        for (int i = start; i < tempStack[(end-start)/2]; i++) {
+            int temp = tempStack[i];
+            tempStack[i] = tempStack[end-i];
+                tempStack[end-i] = temp;
+        }
+        return tempStack;
+
     }
 
 
+    void setNumBreakpoint(int i){ numBreakpoint = i;}
     int getNumBreakpoint(){ return numBreakpoint; }
     int[] getStack(){ return stack; }
+    Node getParent(){return parent;}
+
+
 
 }
